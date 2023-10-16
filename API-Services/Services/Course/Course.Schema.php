ff0d4ffc6eafb150aaa34b509c2254b55b751f90
Maklizy <126657213+Maklizy@users.noreply.github.com>
2023-10-16 20:15:09 +0800

    Update Course.Schema.php

--- API-Services/Services/Course/Course.Schema.php
+++ API-Services/Services/Course/Course.Schema.php
@@ -6,13 +6,12 @@ class Course_Schema
 	static function UpdateProgramEnroll()
 	{
 		$jsonSchema = '{ "Type":  "Object",
-						
 						   "Properties":  {  "student_id":  {  "Type":  "Int"  },
 						   "program_code":  {  "Type":  "Int"  },
 						   "major_code":  {  "Type":  "Int"  },
 						   "others":  {  "Type":  "string"  }}, 
 
-						   "Required":["student_id","program_code","major_code","others",]},
+						"Required":["student_id","program_code","major_code","others",]},
 
 	   }';
 
