c1e0636ca1770c6608c149749207ef4f4587b0f2
Maklizy <126657213+Maklizy@users.noreply.github.com>
2023-10-16 19:51:57 +0800

    deleted program folder
    deleted program folder replace with course folder

---
+++ API-Services/Services/Course/Course.Controller.php
@@ -0,0 +1,22 @@
+<?php
+    
+     require_once ("Course.Model.php");
+      
+    $Method = strtoupper($_SERVER['REQUEST_METHOD']);
+    $Data = file_get_contents('php://input');
+	  
+    $ObjData=json_decode($Data);
+	 
+	 
+	if(strtoupper($Method)=="POST")
+	   { 
+	     if(strtoupper($ObjData->Request)=="UPDATEPROGRAMENROLL")	{Course::UpdateProgramEnroll($ObjData->Record);}
+		 else if(strtoupper($ObjData->Request)=="UPDATECOLLEGE")	{Course::UpdateCollege($ObjData->Record);}
+		 else if(strtoupper($ObjData->Request)=="UPDATEPROGRAM")	{Course::UpdateProgram($ObjData->Record);}
+		 else if(strtoupper($ObjData->Request)=="UPDATEMAJOR")		{Course::UpdateMajor($ObjData->Record);}
+		 else if(strtoupper($ObjData->Request)=="GETREQUEST")		{Course::GetRequest($ObjData->Record);}
+		 else{ echo json_encode(Array("Status"=> "Error: Service request is not valid."), JSON_UNESCAPED_UNICODE);}
+		 
+		 
+	   } else{ echo json_encode(Array("Status"=> "Error: POST method is required in the process."), JSON_UNESCAPED_UNICODE);}
+?>
---
+++ API-Services/Services/Course/Course.Schema.php
@@ -0,0 +1,89 @@
+<?php
+
+class Course_Schema
+{
+
+	static function UpdateProgramEnroll()
+	{
+		$jsonSchema = '{ "Type":  "Object",
+						   "Properties":  {  "enrollment_info":  {  "Type":  "Object",
+						   "Properties":  {  "student_id":  {  "Type":  "Int"  },
+						   "program_code":  {  "Type":  "Int"  },
+						   "major_code":  {  "Type":  "Int"  },
+						   "others":  {  "Type":  "string"  }}, 
+
+						   "Required":["student_id","program_code","major_code","others",]},
+
+					   "Required":  ["enrollment_info"]
+	   }';
+
+		return $jsonSchema;
+	}
+	//--------------------------------------------------------------------------------------------------------
+
+
+	static function SearchProgramEnroll()
+	{
+		$jsonSchema = '{   "Type": "object",
+						   "Properties": { "_student_id": { "Type": "string" }
+									   },
+						   "Required": ["_student_id"]
+					   }';
+		return $jsonSchema;
+	}
+	//--------------------------------------------------------------------------------------------------------
+
+
+	static function UpdateCourse()
+	{
+		$jsonSchema = '{"Type": "object",
+						 "Properties": {
+							"college_code": {"Type": "integer"},
+							"college_description": {"Type": "string"}
+						 }, "Required": ["college_code", "college_description"]
+		}';
+
+		return $jsonSchema;
+	}
+	//--------------------------------------------------------------------------------------------------------
+
+	static function UpdateProgram()
+	{
+		$jsonSchema = '{   "Type": "object",
+						   "Properties": {
+											"program_code": {"Type": "integer"} ,
+											"college_code": { "Type": "integer" },
+											"program_description": { "Type": "string" }
+										},
+							"Required": ["college_code", "program_description"]
+						}';
+		return $jsonSchema;
+	}
+	//--------------------------------------------------------------------------------------------------------
+
+	static function UpdateMajor()
+	{
+		$jsonSchema = '{   "Type": "object",
+						  "Properties": { 
+											"major_code": {"Type": "integer"},
+											"program_code": { "Type": "integer" },
+											"major_description": { "Type": "string" }
+										},
+							"Required": ["program_code", "major_description"]
+						}';
+		return $jsonSchema;
+	}
+
+	//--------------------------------------------------------------------------------------------------------
+
+	static function GetRequest()
+	{
+		$jsonSchema = '{   "Type": "object",
+						  "Properties": { 
+											"request": {"Type": "string"}
+										},
+							"Required": ["request"]
+						}';
+		return $jsonSchema;
+	}
+}
